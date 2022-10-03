0a52b183579ad1ff7737bd619a34f27ded10f28e
DevTeisrud <developer@teisrud.no>
2022-10-03 17:23:13 +0200

    Created folder structure for the application

---
+++ Frontend/Swift/CurriculumVitae/CurriculumVitaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurriculumVitaeApp.swift
+//  CurriculumVitae
+//
+//  Created by Bjørn Kristian Teisrud on 03/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CurriculumVitaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
